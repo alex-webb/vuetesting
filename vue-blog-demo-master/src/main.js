99bdbfd45be7ba8856ba526822f34fe6fac92c8c
Alex Webb <alex.webb@simmons-simmons.com>
2017-11-08 16:00:01 +0000

    vue blog demo

---
+++ vue-blog-demo-master/src/main.js
@@ -0,0 +1,19 @@
+import './startup'
+
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import resource from './resources/resource'
+import * as resources from './resources/implementation'
+
+Vue.config.productionTip = false
+
+Vue.use(resource, {
+  resources,
+  endpoint: '/static/api'
+})
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
